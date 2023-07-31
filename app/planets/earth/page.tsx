8bffd1f4955ff6ccb402930b4ae3ee59ba1c3d29
Max Anderberg <maxanderberg@gmail.com>
2023-07-31 22:50:13 +0200

    init

---
+++ app/planets/earth/page.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function EarthPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between text-white bg-[#070724] bg-gradient-radial absolute inset-0 stars">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex h-full stars">
+        Hello world!
+      </div>
+    </main>
+  )
+}
