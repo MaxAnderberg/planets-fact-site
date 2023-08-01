7c5fbb750433c46e4396927cf79fed2d25f5a6c2
Max Anderberg <maxanderberg@gmail.com>
2023-08-01 22:21:37 +0200

    added styling

--- app/planets/earth/page.tsx
+++ app/planets/earth/page.tsx
@@ -4,22 +4,21 @@ import Fact from '@/components/Fact';
 
 export default function EarthPage() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-between text-white bg-[#070724] bg-gradient-radial h-full bg-stars">
-      <section className='flex'>
-        <img src='/planet-earth.svg' />
+    <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars items-center justify-center pr-[165px]">
+      <section className='flex items-center justify-center mb-[142px]'>
+        <img src='/planet-earth.svg' className='ml-[255px] mr-[195px]' />
         <section className='flex flex-col justify-between'>
-          <h1 className="text-5xl font-bold">Earth</h1>
-          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex h-full flex-col">
+          <h1 className="text-5xl font-bold mb-[23px]">Earth</h1>
+          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex h-full flex-col mb-[24px]">
             Third planet from the Sun and the only known planet to harbor life.
             About 29.2% of Earth's surface is land with remaining 70.8% is
             covered with water. Earth's distance from the Sun, physical properties
             and geological history have allowed life to evolve and thrive.
-            <section>
-              <p>
-                source: <Link href='www.google.se' >Wikipedia</Link>
-              </p>
-            </section>
+            
           </div>
+          <p className='mb-[39px]'>
+              source: <Link href='www.google.se' >Wikipedia</Link>
+            </p>
           <div className='flex flex-col gap-[16px]'>
             <button className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>01 overview</button>
             <button className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>02 internal structure</button>
