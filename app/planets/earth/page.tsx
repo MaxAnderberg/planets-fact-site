bc3b3667841ba69967f644c8bc3930eade24bac6
Max Anderberg <maxanderberg@gmail.com>
2023-08-01 22:45:00 +0200

    added first iteration of handling swapping out text bits

--- app/planets/earth/page.tsx
+++ app/planets/earth/page.tsx
@@ -1,8 +1,28 @@
+'use client'
+import { useState } from 'react';
 import Link from 'next/link';
 import './styles.css';
 import Fact from '@/components/Fact';
 
 export default function EarthPage() {
+  const [selectedButton, setSelectedButton] = useState(1);
+
+  let planetText;
+
+  switch(selectedButton) {
+    case 1:
+      planetText = 'Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth\'s surface is land with remaining 70.8% is covered with water. Earth\'s distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.';
+      break;
+    case 2:
+      planetText = 'This would be the text about the internal structure of the earth.';
+      break;
+    case 3:
+      planetText = 'This would be the text about the surface geology of the earth.';
+      break;
+    default:
+      planetText = '';
+  }
+
   return (
     <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars justify-center pr-[165px]">
       <section className='flex items-center justify-center mb-[142px]'>
@@ -10,18 +30,15 @@ export default function EarthPage() {
         <section className='flex flex-col justify-between'>
           <h1 className="text-5xl font-bold mb-[23px]">Earth</h1>
           <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex h-full flex-col mb-[24px]">
-            Third planet from the Sun and the only known planet to harbor life.
-            About 29.2% of Earth's surface is land with remaining 70.8% is
-            covered with water. Earth's distance from the Sun, physical properties
-            and geological history have allowed life to evolve and thrive.
+            {planetText}
           </div>
           <p className='mb-[39px]'>
             source: <Link href='www.google.se' >Wikipedia</Link>
           </p>
           <div className='flex flex-col gap-[16px]'>
-            <button className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>01 overview</button>
-            <button className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>02 internal structure</button>
-            <button className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>03 surface geology</button>
+            <button onClick={() => setSelectedButton(1)} className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>01 overview</button>
+            <button onClick={() => setSelectedButton(2)} className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>02 internal structure</button>
+            <button onClick={() => setSelectedButton(3)} className='uppercase py-[11px] border border-[#D8D8D8] max-w-[350px]'>03 surface geology</button>
           </div>
         </section>
 
