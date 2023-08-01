e0bf8f784fba1b0aea93297926207d232688ca85
Max Anderberg <maxanderberg@gmail.com>
2023-08-01 23:11:53 +0200

    adjusted styling

--- components/Fact.tsx
+++ components/Fact.tsx
@@ -6,9 +6,9 @@ type Props = {
 
 const Fact = ({title, fact}: Props) => {
 	return (
-		<div className='p-[20px] border-white border-[1px] '>
+		<div className='pl-[23px] py-[20px] border-white border-[1px] lg:w-[255px]'>
 	        <h1 className='uppercase text-[11px] opacity-50'>{title}</h1>
-    	    <h2 className='uppercase text-[40px]'>{fact}</h2>
+    	    <h2 className='uppercase text-[35px]'>{fact}</h2>
       	</div>
 	)	
 }
