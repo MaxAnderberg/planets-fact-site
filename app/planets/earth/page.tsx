5cf6a87f4d085200a1630ee66aeb3d495f0f8287
Max Anderberg <maxanderberg@gmail.com>
2023-08-03 11:13:06 +0200

    fixed endless rendering

--- app/planets/earth/page.tsx
+++ app/planets/earth/page.tsx
@@ -1,37 +1,51 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Fact from '@/components/Fact';
 
 export default function EarthPage() {
-  const [selectedButton, setSelectedButton] = useState(1);
+  const [selectedButton, setSelectedButton] = useState(3);
+  const [showEarthGeology, setDisplayGeology] = useState(false);
 
   let planetText;
-  let planetImage;
+  let planetImage = "/planet-earth.svg";
 
-  switch(selectedButton) {
-    case 1:
-      planetText = 'Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth\'s surface is land with remaining 70.8% is covered with water. Earth\'s distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.';
-      planetImage = '/planet-earth.svg';
-      break;
-    case 2:
-      planetText = "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.";
-      planetImage = '/planet-earth-internal.svg';
-      break;
-    case 3:
-      planetText = "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.";
-      planetImage = '/planet-earth.svg';
-      break;
-    default:
-      planetText = '';
-      planetImage = '/planet-earth.svg';
-  }
+  useEffect(() => {
+    switch(selectedButton) {
+      case 1:
+        planetText = 'Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth\'s surface is land with remaining 70.8% is covered with water. Earth\'s distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.';
+        planetImage = '/planet-earth.svg';
+        setDisplayGeology(false);
+        break;
+      case 2:
+        planetText = "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.";
+        planetImage = '/planet-earth-internal.svg';
+        setDisplayGeology(false);
+        break;
+      case 3:
+        planetText = "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.";
+        planetImage = '/planet-earth.svg';
+        setDisplayGeology(true);
+        break;
+      default:
+        planetText = '';
+        planetImage = '/planet-earth.svg';
+        setDisplayGeology(false);
+    }
+  }, [selectedButton])
 
   return (
     <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars justify-center pr-[165px]">
       <section className='flex items-center justify-center mb-[142px]'>
+        {showEarthGeology ? (
+        <div className='relative ml-[255px] mr-[195px] w-[450px] h-[450px] overflow-visible'>
+          <Image src={planetImage} layout="fill" objectFit="cover" className='absolute bottom-0' alt='image of a cartoony earth'/>
+          <Image src='/geology-earth.png' width='168' height='199' objectFit="cover" className='absolute ml-[32%] mt-[300px]' alt='image of a cartoony earth'/>
+        </div>
+        ) : (
         <Image src={planetImage} width='450' height='450' className='ml-[255px] mr-[195px]' alt='image of a cartoony earth'/>
+        )}  
         <section className='flex flex-col justify-between'>
           <h1 className="text-5xl font-bold mb-[23px]">Earth</h1>
           <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex h-full flex-col mb-[24px]">
