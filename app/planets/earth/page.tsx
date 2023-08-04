4a15ce1fed1a227a0cdc1c28faf79cb6fc46bf43
Max Anderberg <maxanderberg@gmail.com>
2023-08-04 21:17:37 +0200

    added planetText as state so it can persist outside of useEffect

--- app/planets/earth/page.tsx
+++ app/planets/earth/page.tsx
@@ -7,30 +7,30 @@ import { InfoSwitchButton } from '@/components/InfoSwitchButton';
 
 export default function EarthPage() {
   const [selectedButton, setSelectedButton] = useState<number>(1);
-  const [showEarthGeology, setDisplayGeology] = useState(false);
+  const [showEarthGeology, setDisplayGeology] = useState<boolean>(false);
+  const [planetText, setPlanetText] = useState<string>('');
 
-  let planetText;
   let planetImage = "/planet-earth.svg";
 
   useEffect(() => {
     switch(selectedButton) {
       case 1:
-        planetText = 'Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth\'s surface is land with remaining 70.8% is covered with water. Earth\'s distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.';
+        setPlanetText('Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth\'s surface is land with remaining 70.8% is covered with water. Earth\'s distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.');
         planetImage = '/planet-earth.svg';
         setDisplayGeology(false);
         break;
       case 2:
-        planetText = "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.";
+        setPlanetText("Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.");
         planetImage = '/planet-earth-internal.svg';
         setDisplayGeology(false);
         break;
       case 3:
-        planetText = "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.";
+      setPlanetText("The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.");
         planetImage = '/planet-earth.svg';
         setDisplayGeology(true);
         break;
       default:
-        planetText = '';
+        setPlanetText('');
         planetImage = '/planet-earth.svg';
         setDisplayGeology(false);
     }
