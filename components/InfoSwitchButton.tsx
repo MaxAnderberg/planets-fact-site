3bcad2cfb97010491e9b2ba81cebcce18d7fc25f
Max Anderberg <maxanderberg@gmail.com>
2023-08-05 16:34:01 +0200

    added mobile layout

--- components/InfoSwitchButton.tsx
+++ components/InfoSwitchButton.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export const InfoSwitchButton = ({ section, title, setSelectedButton, selectedButton, id }: Props) => {
   return (
-    <button onClick={() => setSelectedButton(id)} className={`uppercase text-left pl-[24px] py-[11px] border border-[#D8D8D8] max-w-[350px] ${selectedButton == id ? 'bg-[#6D2ED5] border-none' : ''} hover:bg-[#D8D8D8] hover:bg-opacity-20`}>
-      <p>{section}</p>
+    <button onClick={() => setSelectedButton(id)} className={`flex text-[9px] uppercase text-center md:border md:border-[#D8D8D8] md:max-w-[350px] ${selectedButton == id ? 'bg-[#6D2ED5] border-none' : ''} hover:bg-[#D8D8D8] hover:bg-opacity-20`}>
+      <p className="hidden md:visible">{section}</p>
       <p>{title}</p>
     </button>
   )
