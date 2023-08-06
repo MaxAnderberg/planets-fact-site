360a7e104f8bc1c5817dfcf54dcc707fa9e18744
Max Anderberg <maxanderberg@gmail.com>
2023-08-06 17:05:08 +0200

    formatting

--- components/Fact.tsx
+++ components/Fact.tsx
@@ -1,16 +1,15 @@
-
 type Props = {
 	title: string
 	fact: string
 }
 
-const Fact = ({title, fact}: Props) => {
+const Fact = ({ title, fact }: Props) => {
 	return (
-		<div className='flex justify-between items-center px-[24px] py-[9px] border-white border-[1px] lg:w-[255px] w-full'>
-	        <h1 className='uppercase text-[11px] opacity-50'>{title}</h1>
-    	    <h2 className='uppercase text-[20px]'>{fact}</h2>
-      	</div>
-	)	
+		<div className='flex justify-between items-center px-[24px] py-[9px] border-white border-[1px] lg:w-[255px] w-full border-opacity-20'>
+			<h1 className='uppercase text-[11px] opacity-50'>{title}</h1>
+			<h2 className='uppercase text-[20px]'>{fact}</h2>
+		</div>
+	)
 }
 
 export default Fact;
