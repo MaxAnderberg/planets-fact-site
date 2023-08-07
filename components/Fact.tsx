6f360d18ec23f0587316ac901cd4174058d09c2d
Max Anderberg <maxanderberg@gmail.com>
2023-08-07 18:58:04 +0200

    added correct font

--- components/Fact.tsx
+++ components/Fact.tsx
@@ -6,8 +6,8 @@ type Props = {
 const Fact = ({ title, fact }: Props) => {
 	return (
 		<div className='flex justify-between items-center px-[24px] py-[9px] border-white border-[1px] lg:w-[255px] w-full border-opacity-20'>
-			<h1 className='uppercase text-[11px] opacity-50'>{title}</h1>
-			<h2 className='uppercase text-[20px]'>{fact}</h2>
+			<h1 className='uppercase text-[11px] opacity-50 font-spartan'>{title}</h1>
+			<h2 className='uppercase text-[20px] font-antonio'>{fact}</h2>
 		</div>
 	)
 }
