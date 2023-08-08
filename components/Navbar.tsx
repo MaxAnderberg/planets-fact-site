dc85a0a7039ea17549827e86e9c59939d82bfea1
Max Anderberg <maxanderberg@gmail.com>
2023-08-08 20:59:54 +0200

    added a hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,16 @@ import { planetsData } from "@/app/data/planets";
 const Navbar = () => {
   console.log(planetsData);
   return (
-    <nav>
+    <nav className='bg-[#070724] text-white'>
       <div className='flex justify-between'>
       <p className='uppercase font-antonio text-[28px]'>
         The planets
       </p>
-      burger menu
+      <div className='w-[32px] h-[20px] gap-[4px] flex flex-col cursor-pointer'>
+        <div className='bg-white w-full h-[4px]'></div>
+        <div className='bg-white w-full h-[4px]'></div>
+        <div className='bg-white w-full h-[4px]'></div>
+      </div>
       </div>
       <ul>
         {planetsData.map((planet) => (
