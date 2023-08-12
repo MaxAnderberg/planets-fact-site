5f38df58d5550c70f6ec318220c730a2cd78ac3b
Max Anderberg <maxanderberg@gmail.com>
2023-08-12 14:26:46 +0200

    adapted for tablet layout

--- components/Fact.tsx
+++ components/Fact.tsx
@@ -1,15 +1,15 @@
 type Props = {
-	title: string
-	fact: string | undefined
+  title: string
+  fact: string | undefined
 }
 
 const Fact = ({ title, fact }: Props) => {
-	return (
-		<div className='flex justify-between items-center px-[24px] py-[9px] border-white border-[1px] lg:w-[255px] w-full border-opacity-20'>
-			<h1 className='uppercase text-[11px] opacity-50 font-spartan'>{title}</h1>
-			<h2 className='uppercase text-[20px] font-antonio'>{fact}</h2>
-		</div>
-	)
+  return (
+    <div className='flex justify-between items-center px-[24px] py-[9px] border-white border-[1px] md:w-[164px] md:px-0 md:pl-[16px] md:h-[88px] md:items-start md:justify-start md:gap-1 lg:w-[255px] w-full border-opacity-20 md:flex-col'>
+      <h1 className='uppercase text-[11px] md:font-bold opacity-50 font-spartan'>{title}</h1>
+      <h2 className='uppercase text-[20px] md:text-[24pxk] font-antonio'>{fact}</h2>
+    </div>
+  )
 }
 
 export default Fact;
