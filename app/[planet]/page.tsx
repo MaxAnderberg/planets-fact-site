5f134e8a3e95eff7a46ca7978948efdbeff150b0
Max Anderberg <maxanderberg@gmail.com>
2023-08-12 14:35:09 +0200

    fixed info buttons for tablet layout

--- app/[planet]/page.tsx
+++ app/[planet]/page.tsx
@@ -68,9 +68,9 @@ export default function PlanetPage({ params }: { params: { planet: string } }) {
   }, [selectedButton])
 
   return (
-    <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars">
+    <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars md:px-[39px]">
       <section className='flex flex-col items-center justify-center md:grid md:grid-rows-1 md:grid-cols-2'>
-        <div className='flex justify-center gap-[16px] border-y md:border-none mt-[10px] w-full pt-[20px] mb-[64px] md:flex-col md:row-start-2 md:row-end-3 md:col-start-2'>
+        <div className='flex justify-center gap-[16px] border-y md:border-none mb-[0] w-full pt-[20px] mb-[64px] md:mt-[15px] md:flex-col md:row-start-2 md:row-end-3 md:col-start-2'>
           <InfoSwitchButton section='01' title='overview' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={1} />
           <InfoSwitchButton section='02' title='internal structure' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={2} />
           <InfoSwitchButton section='03' title='surface geology' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={3} />
@@ -86,8 +86,8 @@ export default function PlanetPage({ params }: { params: { planet: string } }) {
           )}
         </div>
         <div className='md:grid md:row-start-2 md:col-start-1'>
-          <h1 className="text-5xl font-bold mb-[16px] text-[40px] font-antonio uppercase text-center mt-[67px]">{params.planet}</h1>
-          <div className="z-10 w-full max-w-5xl items-center justify-between text-sm text-center lg:flex h-full flex-col mb-[24px] font-spartan leading-6">
+          <h1 className="text-5xl font-bold mb-[16px] md:mb-[24px] text-[40px] font-antonio uppercase text-center md:text-left mt-[67px]">{params.planet}</h1>
+          <div className="z-10 w-full max-w-5xl items-center justify-between text-sm text-center md:text-left lg:flex h-full flex-col mb-[24px] font-spartan leading-6">
             {planetText}
           </div>
           <div className='mb-[39px] font-spartan flex'>
