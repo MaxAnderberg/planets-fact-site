0ebf3ff4e9924336bf05578f9cd57fb9b39ea0c3
Max Anderberg <maxanderberg@gmail.com>
2023-08-16 22:49:33 +0200

    fixed size for large screens

--- components/Fact.tsx
+++ components/Fact.tsx
@@ -8,8 +8,8 @@ type Props = {
 const spartan = League_Spartan({ subsets: ['latin'] })
 const Fact = ({ title, fact }: Props) => {
   return (
-    <div className='flex justify-between items-center px-[24px] py-[9px] border-white border-[1px] md:w-[164px] md:px-0 md:pl-[16px] md:h-[88px] md:items-start md:justify-start md:gap-1 lg:w-[255px] w-full border-opacity-20 md:flex-col'>
-      <h1 className='uppercase text-[11px] md:font-bold opacity-50 font-spartan' style={spartan.style}>{title}</h1>
+    <div className='flex justify-between items-center px-[24px] py-[9px] border-white border-[1px] md:w-[164px] md:px-0 md:pl-[16px] md:h-[88px] md:items-start md:justify-start md:gap-1 lg:w-[255px] w-full border-opacity-20 md:flex-col lg:h-[128px] lg:pb-[27px]'>
+      <h1 className='uppercase text-[11px] md:font-bold opacity-50 font-spartan lg:text-[12px]' style={spartan.style}>{title}</h1>
       <h2 className='uppercase text-[20px] md:text-[24px] lg:text-[40px]'>{fact}</h2>
     </div>
   )
