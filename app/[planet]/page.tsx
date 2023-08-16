9aebcb766cc18767bded75714e65c5ebc3990fad
Max Anderberg <maxanderberg@gmail.com>
2023-08-16 21:33:39 +0200

    formatting

--- app/[planet]/page.tsx
+++ app/[planet]/page.tsx
@@ -75,7 +75,7 @@ export default function PlanetPage({ params }: { params: { planet: string } }) {
     <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars md:px-[39px] md:pb-[36px] lg:px-[165px] lg:pb-[56px] ">
       <section>
         <section className='flex flex-col items-center justify-center md:grid md:grid-rows-1 md:grid-cols-2 lg:w-full lg:max-w-[1440px] lg:mb-[87px]'>
-          <div className='flex justify-center gap-[16px] border-y md:border-none w-full pt-[20px] mb-[64px] md:mt-[15px] md:flex-col md:row-start-2 md:row-end-3 lg:items-end lg:justify-start md:col-start-2 md:items-end lg:col-start-2 lg:mt-0 lg:pt-0'>
+          <div className='flex justify-center gap-[16px] border-y md:border-none w-full pt-[20px] mb-[64px] md:mt-[15px] md:flex-col md:row-start-2 md:row-end-3 lg:items-end lg:justify-start md:col-start-2 md:items-end lg:col-start-2 lg:mt-0 lg:pt-0 lg:mb-0'>
             <InfoSwitchButton section='01' title='overview' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={1} />
             <InfoSwitchButton section='02' title='internal structure' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={2} />
             <InfoSwitchButton section='03' title='surface geology' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={3} />
