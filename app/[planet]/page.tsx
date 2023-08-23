3e6ad1b99870ba99b78c7a74e14dbc571db6add0
Max <maxanderberg@gmail.com>
2023-08-23 22:18:14 +0200

    formatted and positioned source on sm and md

--- app/[planet]/page.tsx
+++ app/[planet]/page.tsx
@@ -101,45 +101,45 @@ export default function PlanetPage({ params }: { params: { planet: string } }) {
   }, []);
 
   return (
-    <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars md:px-[39px] md:pb-[36px] lg:px-[165px] lg:pb-[56px] ">
-      <section>
-        <section className='flex flex-col items-center justify-center md:grid md:grid-rows-1 md:grid-cols-2 lg:w-full lg:max-w-[1440px]'>
-          <div className='flex justify-center gap-[16px] border-y border-white border-opacity-25 md:border-none w-full pt-[20px] mb-[64px] md:mt-[85px] md:flex-col md:row-start-2 md:row-end-3 lg:items-end lg:justify-start md:col-start-2 md:items-end lg:col-start-2 lg:mt-0 lg:pt-0 lg:mb-0'>
-            <InfoSwitchButton section='01' title='overview' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={1} />
-            <InfoSwitchButton section='02' title='internal structure' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={2} />
-            <InfoSwitchButton section='03' title='surface geology' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={3} />
-          </div>
-          <div className='px-[24px] flex items-center flex-col md:mt-[90px] md:mb-[12px] lg:mb-0 md:row-start-1 md:row-end-2 col-span-2 lg:col-start-1 lg:col-end-2 lg:row-span-2 lg:mt-[117px] lg:px-0 lg:justify-end lg:items-end'>
-            {showEarthGeology ? (
-              <div className='relative overflow-visible m-auto flex justify-center items-center md:m-0'>
-                <Image src={planetImage} width={imageWidth} height={imageWidth} alt='image of a cartoony earth' />
-                  <div className='absolute flex items-center justify-center inset-0 m-auto top-[80%] lg:top-[80%] w-[50%] md:w-[107px] lg:w-[163px]'>
-                  <Image src={geologyImage} width={163} height={199} alt="image of planets's surface"/>
-                </div>
-              </div>
-            ) : (
-              <Image src={planetImage} width={imageWidth} height={imageWidth} alt='image of a cartoony earth' />
-            )}
-          </div>
-          <div className='px-[24px] md:px-0 md:grid md:row-start-2 md:col-start-1 lg:col-start-2 lg:row-start-1 lg:flex lg:flex-col lg:items-end'>
-            <h1 className="text-5xl font-bold mb-[16px] md:mb-[24px] text-[40px] font-antonio uppercase text-center md:text-left mt-[67px] lg:w-[350px] lg:text-left lg:text-[80px] lg:mt-[126px] lg:pb-[10px]" style={antonio.style}>{params.planet}</h1>
-            <div className="z-10 w-full max-w-5xl md:w-[339px]  items-center justify-between text-sm text-center md:text-left lg:flex h-full flex-col mb-[10px] md:mb-[24px] font-spartan leading-6 lg:w-[350px] lg:mb-[24px]" style={spartan.style}>
-              {planetText}
-            </div>
-            <div className='mb-[28px] md:mb-[39px] font-spartan flex lg:mb-[39px] lg:w-[350px] text-left'>
-              <p className='opacity-50'>Source :&nbsp;</p><Link href={planetWikipedia} className='underline font-bold text-[#838391] flex gap-[8px]'>Wikipedia <LinkIcon width={12} height={12} /></Link>
-            </div>
-          </div>
-        </section>
+        <main className="flex flex-col min-h-screen items-center text-white bg-[#070724] bg-gradient-radial h-full bg-stars md:px-[39px] md:pb-[36px] lg:px-[165px] lg:pb-[56px] ">
+            <section>
+                <section className='flex flex-col items-center justify-center md:grid md:grid-rows-1 md:grid-cols-2 lg:w-full lg:max-w-[1440px]'>
+                    <div className='flex justify-center gap-[16px] border-y border-white border-opacity-25 md:border-none w-full pt-[20px] mb-[64px] md:mt-[85px] md:flex-col md:row-start-2 md:row-end-3 lg:items-end lg:justify-start md:col-start-2 md:items-end lg:col-start-2 lg:mt-0 lg:pt-0 lg:mb-0'>
+                        <InfoSwitchButton section='01' title='overview' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={1} />
+                        <InfoSwitchButton section='02' title='internal structure' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={2} />
+                        <InfoSwitchButton section='03' title='surface geology' selectedButton={selectedButton} setSelectedButton={setSelectedButton} id={3} />
+                    </div>
+                    <div className='px-[24px] flex items-center flex-col md:mt-[90px] md:mb-[12px] lg:mb-0 md:row-start-1 md:row-end-2 col-span-2 lg:col-start-1 lg:col-end-2 lg:row-span-2 lg:mt-[117px] lg:px-0 lg:justify-end lg:items-end'>
+                        {showEarthGeology ? (
+                            <div className='relative overflow-visible m-auto flex justify-center items-center md:m-0'>
+                                <Image src={planetImage} width={imageWidth} height={imageWidth} alt='image of a cartoony earth' />
+                                <div className='absolute flex items-center justify-center inset-0 m-auto top-[80%] lg:top-[80%] w-[50%] md:w-[107px] lg:w-[163px]'>
+                                    <Image src={geologyImage} width={163} height={199} alt="image of planets's surface" />
+                                </div>
+                            </div>
+                        ) : (
+                            <Image src={planetImage} width={imageWidth} height={imageWidth} alt='image of a cartoony earth' />
+                        )}
+                    </div>
+                    <div className='px-[24px] md:px-0 md:grid md:row-start-2 md:col-start-1 lg:col-start-2 lg:row-start-1 lg:flex lg:flex-col lg:items-end'>
+                        <h1 className="text-5xl font-bold mb-[16px] md:mb-[24px] text-[40px] font-antonio uppercase text-center md:text-left mt-[67px] lg:w-[350px] lg:text-left lg:text-[80px] lg:mt-[126px] lg:pb-[10px]" style={antonio.style}>{params.planet}</h1>
+                        <div className="z-10 w-full max-w-5xl md:w-[339px]  items-center justify-between text-sm text-center md:text-left lg:flex h-full flex-col mb-[10px] md:mb-[24px] font-spartan leading-6 lg:w-[350px] lg:mb-[24px]" style={spartan.style}>
+                            {planetText}
+                        </div>
+                        <div className='mb-[28px] md:mb-[39px] font-spartan flex lg:mb-[39px] lg:w-[350px] text-left items-center justify-center md:justify-start'>
+                            <p className='opacity-50  text-[12px] lg:text-[14px'>Source :&nbsp;</p><Link href={planetWikipedia} className='underline font-bold text-[12px] lg:text-[14px] text-[#838391] flex gap-[8px]'>Wikipedia <LinkIcon width={12} height={12} /></Link>
+                        </div>
+                    </div>
+                </section>
 
-        <section className='flex flex-col gap-[8px] w-full px-[24px] md:flex-row md:justify-between md:px-0 lg:gap-[30px] lg:justify-normal lg:mt-[87px]' style={antonio.style}>
-          <Fact title='Rotation time' fact={planet?.rotation} />
-          <Fact title='Revolution time' fact={planet?.revolution} />
-          <Fact title='Radius' fact={planet?.radius} />
-          <Fact title='AVERAGE TEMP.' fact={planet?.temperature} />
-        </section>
-      </section>
+                <section className='flex flex-col gap-[8px] w-full px-[24px] md:flex-row md:justify-between md:px-0 lg:gap-[30px] lg:justify-normal lg:mt-[87px]' style={antonio.style}>
+                    <Fact title='Rotation time' fact={planet?.rotation} />
+                    <Fact title='Revolution time' fact={planet?.revolution} />
+                    <Fact title='Radius' fact={planet?.radius} />
+                    <Fact title='AVERAGE TEMP.' fact={planet?.temperature} />
+                </section>
+            </section>
 
-    </main>
-  )
+        </main>
+    )
 }
